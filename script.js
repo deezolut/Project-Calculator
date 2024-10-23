789fe0f5b1ef335b0812147612c0262c3496d5c4
deezolut <deezolut@gmail.com>
2024-10-23 09:27:47 +0300

    Create calculator basic layout

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const log = console.log;
+const table = console.table;
 
 function addition(array) {
     return array.reduce((total, num) => {
@@ -27,4 +28,36 @@ function divide(array) {
         return total;
     });
 };
+
+
+let num1
+let operator
+let num2
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+': 
+            return addition([num1, num2]);
+
+        case '-':
+            return subtraction([num1, num2]);
+
+        case '*':
+            return multiply([num1, num2]);
+
+        case '/':
+            return divide([num1, num2]);
+
+    };
+};
+
+
+const buttons = document.querySelector('#container-of-buttons');
+
+for (let i = 1; i < 20; i++) {
+    let button = document.createElement('div');
+    button.setAttribute('id', `btn${i}`);
+    button.classList.add('button');
+    buttons.appendChild(button);
+}
 
