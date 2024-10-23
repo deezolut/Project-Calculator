9016334192122e4eca5b064a1f42516a546330ba
deezolut <deezolut@gmail.com>
2024-10-23 08:12:17 +0300

    Create multiplication and division function

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const log = console.log
+const log = console.log;
 
 function addition(array) {
     return array.reduce((total, num) => {
         total += num;
         return total;
-    }, 0)
-}
+    }, 0);
+};
 
 function subtraction(array) {
     return array.reduce((total, num) => {
         total -= num;
         return total;
-    })
-}
+    });
+};
+
+function multiply(array) {
+    return array.reduce((total, num) => {
+        total *= num;
+        return total;
+    });
+};
+
+function divide(array) {
+    return array.reduce((total, num) => {
+        total /= num;
+        return total;
+    });
+};
 
