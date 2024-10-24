79fcaad2dc9e431c69cd0259b1138371b999d910
deezolut <deezolut@gmail.com>
2024-10-24 17:24:55 +0300

    Finish layout and styling

--- script.js
+++ script.js
@@ -61,3 +61,86 @@ for (let i = 1; i < 20; i++) {
     buttons.appendChild(button);
 }
 
+for (let i = 1; i < 20; i++) {
+    let button = document.querySelector(`#btn${i}`);
+
+    switch (i) {
+
+        case 1:
+            button.textContent = 'AC';
+            button.style.color = 'white'
+            break;
+        case 2:
+            button.textContent = 'DEL';
+            button.style.color = 'white'
+            break;
+        case 3:
+            button.textContent = '%';
+            button.style.color = 'white'
+            break;
+        case 4:
+            button.textContent = '+';
+            button.style.color = 'white'
+            break;
+        case 5:
+            button.textContent = '1';
+            button.style.color = 'white'
+            break;
+        case 6:
+            button.textContent = '2';
+            button.style.color = 'white'
+            break;
+        case 7:
+            button.textContent = '3';
+            button.style.color = 'white'
+            break;
+        case 8:
+            button.textContent = '-';
+            button.style.color = 'white'
+            break;
+        case 9:
+            button.textContent = '4';
+            button.style.color = 'white'
+            break;
+        case 10:
+            button.textContent = '5';
+            button.style.color = 'white'
+            break;
+        case 11:
+            button.textContent = '6';
+            button.style.color = 'white'
+            break;
+        case 12:
+            button.textContent = '/';
+            button.style.color = 'white'
+            break;
+        case 13:
+            button.textContent = '7';
+            button.style.color = 'white'
+            break;
+        case 14:
+            button.textContent = '8';
+            button.style.color = 'white'
+            break;
+        case 15:
+            button.textContent = '9';
+            button.style.color = 'white'
+            break;
+        case 16:
+            button.textContent = '*';
+            button.style.color = 'white'
+            break;
+        case 17:
+            button.textContent = '0';
+            button.style.color = 'white'
+            break;
+        case 18:
+            button.textContent = '.';
+            button.style.color = 'white'
+            break;
+        case 19:
+            button.textContent = '=';
+            button.style.color = 'white'
+            break;
+    }
+}
